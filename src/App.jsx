86a96c21c9acddd066cfdd58d1f537e97a37878c
Gautam Kumar <=>
2025-09-17 22:51:19 +0530

    here is now divided and add hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,11 @@
 import React from 'react'
+import Header from './components/header'
 
 const App = () => {
   return (
     <>
-
-   <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
-      <div class="container-fluid mx-3">
-        <a class="navbar-brand" href="#">Gautam Arya</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="#">Projects</a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="#">Skills & Experience</a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="#">Education</a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-          <a class="btn resume-btn" target="_blank" href="PARTY INVITATION.pdf"
-            >Get Resume</a
-          >
-        </div>
-      </div>
-    </nav>
-
-
+     <Header />
     </>
-   
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+
+
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+                <div class="container-fluid mx-3">
+                    <a class="navbar-brand" href="#">Gautam Arya</a>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item mx-2">
+                                <a class="nav-link" href="#home">Home</a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link" href="#projects">Projects</a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link" href="#skills">Skills & Experience</a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link" href="#education">Education</a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                        <a class="btn resume-btn" target="_blank" href="PARTY INVITATION.pdf"
+                        >Get Resume</a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
